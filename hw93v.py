570ad9595c5f56b5277b2c25cc2c16d054735e0f
varvara-spb99 <59234514+varvara-spb99@users.noreply.github.com>
2020-01-09 01:04:56 +0200

    Problem 48

---
+++ hw93v.py
@@ -0,0 +1,16 @@
+"""Решить несколько задач из projecteuler.net
+
+Решения должны быть максимально лаконичными, и использовать list comprehensions.
+
+problem9 - list comprehension : one line
+problem6 - list comprehension : one line
+problem48 - list comprehension : one line
+problem40 - list comprehension
+
+Problem48:
+The series, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
+
+Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000."""
+
+print("Problem 48: ")
+print(sum([i**i for i in range(1,1001)])%(10**10))
