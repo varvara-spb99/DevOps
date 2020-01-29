36271839067960c71df9a3650e960a08894fc5e3
varvara-spb99 <59234514+varvara-spb99@users.noreply.github.com>
2020-01-29 14:07:19 +0200

    Homework 6 remade

--- hw6v.py
+++ hw6v.py
@@ -3,22 +3,12 @@ Find the sum of all numbers, less than one million, which are palindromic in
 base 10 and base 2. (Please note that the palindromic number,
 in either base, may not include leading zeros.)"""
 
-
 def is_palindrome(num):
     return str(num) == str(num)[::-1]
-
-def dec_to_bin(num):
-    b = ''
-    while num > 0:
-        b = str(num % 2) + b
-        num = num // 2
-    return b
 
 count = 1000000
 sum = 0
 for i in range(count):
-    if is_palindrome(i) == True and is_palindrome(dec_to_bin(i)) == True:
-        #print(i)
-        #print(dec_to_bin(i))
+    if is_palindrome(i) == True and is_palindrome(bin(i)[2:]) == True:
         sum += i
 print("Sum =", sum)
