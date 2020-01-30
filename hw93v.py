9d6be4fd23fbafe3cdaa171f6f0b6c820c1f4caf
varvara-spb99 <59234514+varvara-spb99@users.noreply.github.com>
2020-01-30 23:49:52 +0200

    Homework 93v changed

--- hw93v.py
+++ hw93v.py
@@ -13,4 +13,4 @@ The series, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
 Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000."""
 
 print("Problem 48: ")
-print(sum([i**i for i in range(1,1001)])%(10**10))
+print(str(sum([i**i for i in range(1,1001)]))[-10:])
