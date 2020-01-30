fd8a7f942f6d8a4cf1a1bc0554f5f41e37f9ac4e
varvara-spb99 <59234514+varvara-spb99@users.noreply.github.com>
2020-01-31 01:18:34 +0200

    Homework 12 changed

--- hw12v.py
+++ hw12v.py
@@ -2,16 +2,18 @@
 элементы последовательности Фиббоначи:
 1 1 2 3 5 8 13 21 34 55 ......."""
 
+
 def fib(n):
     a = 1
     b = 1
     if n >= 1:
         print(str(a))
-    if n >= 2:
-        print(str(b))
-    for i in range(n-2):
-        a, b = b, a + b
-        print(b)
+        if n >= 2:
+            print(str(b))
+            if n>=3:
+                for i in range(n-2):
+                    a, b = b, a + b
+                    print(b)
     return
 
 fib(10)
