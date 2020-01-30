f743b8ef43fe4a6ac39717471889cd878f8d2ac0
varvara-spb99 <59234514+varvara-spb99@users.noreply.github.com>
2020-01-31 00:52:27 +0200

    Homework 94v changed

--- hw94v.py
+++ hw94v.py
@@ -18,24 +18,17 @@ If dn represents the n-th digit of the fractional part, find the value of the fo
 
 d1 × d10 × d100 × d1000 × d10000 × d100000 × d1000000
 185185"""
-from itertools import repeat
+#from itertools import repeat
 
 print("Problem 40: ")
-string = 1
-print(int(''.join([str(string+a) for a in range(200000)])[0])*int(''.join([str(string+a) for a in range(200000)])[9])*int(''.join([str(string+a) for a in range(200000)])[99])
-      *int(''.join([str(string+a) for a in range(200000)])[999])*int(''.join([str(string+a) for a in range(200000)])[9999])
-      *int(''.join([str(string+a) for a in range(200000)])[99999])*int(''.join([str(string+a) for a in range(200000)])[999999]))
-
-#другое решение без list comprehension
-string = ''
-a = 1
-while len(string)<1000000:
-    string = string + str(a)
-    a +=1
-#print(string)
-res = int(string[0])*int(string[9])*int(string[99])*int(string[999])*int(string[9999])*int(string[99999])*int(string[999999])
-#print(string[0], string[9], string[99], string[999], string[9999], string[99999], string[999999] )
+string = ''.join([str(1+a) for a in range(200000)])
+lst = [int(string[a]) for a in [0, 9, 99, 999, 9999, 99999, 999999]]
+res = 1
+for i in lst:
+    res = res * i
 print(res)
+
+
 
 
 
