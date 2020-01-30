13e38b8684f8bc8379eb68f0a46f27480f03bbb9
varvara-spb99 <59234514+varvara-spb99@users.noreply.github.com>
2020-01-31 01:28:22 +0200

    Homework 12 changed

--- hw12v.py
+++ hw12v.py
@@ -4,16 +4,22 @@
 
 
 def fib(n):
+    lst=[]
     a = 1
     b = 1
-    if n >= 1:
-        print(str(a))
-        if n >= 2:
-            print(str(b))
-            if n>=3:
-                for i in range(n-2):
-                    a, b = b, a + b
-                    print(b)
-    return
+    if n==1:
+        lst.append(a)
+        return lst
+    elif n==2:
+        lst.append(a)
+        lst.append(b)
+        return lst
+    elif n>=3:
+        lst.append(a)
+        lst.append(b)
+        for i in range(n-2):
+            a, b = b, a + b
+            lst.append(b)
+    return lst
 
-fib(10)
+print(fib(10))
